790e2b349e6c22dcb1bc524988b28dbe3a76992e
Brett Swan <brettswan523@gmail.com>
2017-12-05 22:53:25 -0800

    Changes to worker.h

--- Worker.cpp
+++ Worker.cpp
@@ -3,3 +3,4 @@
 //
 
 #include "Worker.h"
+
--- Worker.h
+++ Worker.h
@@ -10,21 +10,32 @@
 
 class Worker {
 
- void run() {
-            for (int i = 0; i < Iterations; ++i) {
-                Semaphore.tryAcquire();
-                for(int j = 0; j < 160 / Iterations; ++j)    {
-                    doWork();
-                    Worker_Counter.incrementAndGet();
+    Semaphore semaphore;
+    int Iterations;
+    int Worker_Counter;
+    int matrix[1000][1000];
+
+public:
+    Worker(Semaphore semaphore, int Iterations, int Worker_Counter){
+        Iterations = Iterations;
+        Worker_Counter = Worker_Counter;
+        semaphore = semaphore;
+
+    void run() {
+        for (int i = 0; i < Iterations; ++i) {
+            Semaphore.tryAcquire();
+            for(int j = 0; j < 160 / Iterations; ++j)    {
+                doWork();
+                Worker_Counter.incrementAndGet();
                 }
             }
         }
     }
 
  void doWork()    {
-        for (int i = 0; i < 100; ++i) {
-            matrix[new Random().nextInt(100)][new Random().nextInt(100)]
-            = matrix[new Random().nextInt(100)][new Random().nextInt(100)] * 5.2323223586654321;
+        for (int i = 0; i < 1000; ++i) {
+            matrix[rand()%999][rand()%999]
+            = matrix[rand()%999][rand()%999] * 5.2323223586654321;
         }
     }
 };

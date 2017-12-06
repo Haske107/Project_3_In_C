6239ce28adc5ba4a03bfc0841637667631c8ba61
Jeff <Haske107@mail.chapman.edu>
2017-12-05 22:52:46 -0800

    j

--- Scheduler.h
+++ Scheduler.h
@@ -6,7 +6,41 @@
 #define PROJECT_3_SCHEDULER_H
 
 
+#include "Worker.h"
+
 class Scheduler {
+
+    // PROGRAM COUNTER
+    int Program_Counter;
+
+    // COUNTERS
+    int Worker1_Counter;
+    int Worker2_Counter;
+    int Worker4_Counter;
+    int Worker16_Counter;
+
+    // OVERRUN COUNTERS
+    int Worker1_Overrun_Counter;
+    int Worker2_Overrun_Counter;
+    int Worker4_Overrun_Counter;
+    int Worker16_Overrun_Counter;
+
+    // TIMED SEMAPHORE
+     Semaphore Scheduler_Semaphore;
+
+    // WORKER SEMAPHORES
+     Semaphore Worker1_Semaphore;
+     Semaphore Worker2_Semaphore;
+     Semaphore Worker4_Semaphore;
+     Semaphore Worker16_Semaphore;
+
+    // WORKERS
+     Worker W1;
+     Worker W2;
+     Worker W4;
+     Worker W16;
+
+
 
 };
 
